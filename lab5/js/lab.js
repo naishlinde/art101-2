361d22ce3a9a50bf28b35734c42eff152b4893eb
naishlinde <ilinde@ucsc.edu>
2021-01-27 21:44:38 -0500

    -

---
+++ lab5/js/lab.js
@@ -0,0 +1,14 @@
+var make = "Volkswagen";
+var model = "Beetle";
+var color = "Chartreuse";
+var year = 1965;
+var currentyear = 2021;
+
+//push variables to html
+
+document.write("Make: " + make + "<br>");
+document.write("Model: " + model + "<br>");
+document.write("Color: " + color + "<br>");
+document.write("Year: " + year + "<br>");
+document.write("Age: " + (currentyear - year) + "<br>");
+document.write("<br>")
